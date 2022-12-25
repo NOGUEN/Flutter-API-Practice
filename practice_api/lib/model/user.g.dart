46a9108f04c8ebff71314d07d81b87dfc9626317
NOGUEN <nhg1113@naver.com>
2022-12-25 22:20:55 +0900

    Model : UserModel Class

---
+++ practice_api/lib/model/user.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+      id: json['id'] as int,
+      username: json['username'] as String?,
+      phoneNumber: json['phoneNumber'] as String,
+    );
+
+Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
+      'id': instance.id,
+      'username': instance.username,
+      'phoneNumber': instance.phoneNumber,
+    };
